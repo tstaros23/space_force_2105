e705129cbe95cfe0e89b336456c549fe2b933f69
Ted Staros <tstaros23@gmail.com>
2021-06-03 09:24:31 -0600

    Add testingspec for iteration 1

---
+++ spec/person_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './lib/person'
+
+describe do Person
+  before :each do
+    kathy = Person.new('Kathy Chan', 10)
+  end
+  it 'exists' do
+    expect(kathy).to be_a(Person)
+  end
+
+  it 'has attributes' do
+    expect(kathy.name).to eq("Kathy Chan")
+    expect(kathy.experience).to eq(10)
+    expect(kathy.specialities).to eq([])
+  end
+
+  it 'adds specialities' do
+    kathy.add_specialty(:astrophysics)
+    kathy.add_specialty(:quantum_mechanics)
+
+    expect(kathy.specialities).to eq([:astrophysics, :quantum_mechanics])
+  end
+
+end
---
+++ spec/spacecraft_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/spacecraft'
+
+describe do Spacecraft
+  before :each do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+  end
+
+  it 'exists' do
+    expect(daedalus).to be_a(Spacecraft)
+  end
+
+  it 'has attributes' do
+    expect(daedalus.name).to eq("Daedalus")
+    expect(daedalus.fuel).to eq(400)
+  end
+
+end
